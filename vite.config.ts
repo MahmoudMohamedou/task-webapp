62de1cf7cd4b2940205eff5713272e0411f3cff3
MOHAMEDOU Mahmoud <mahmoudyahya3230@gmail.com>
2024-06-03 01:10:17 +0200

    fix hostname

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: ".env.production",
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: "localhost",
+    port: 5000,
+  },
+  preview: {
+    host: process.env.VITE_HOST,
+    port: +process.env.VITE_PORT,
+  },
+
   plugins: [react()],
 });
